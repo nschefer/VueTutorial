3aac61e450ff56256c7303b314b49558858bf9d6
Noah Schefer <nbschefer@gmail.com>
2019-12-09 17:14:39 -0500

    Lesson 7 Complete

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    inStock: false,
-    inventory: 0,
+    onSale: true,
+    selectedVariant: 0,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green-onWhite.jpg'
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -27,8 +29,22 @@ const app = new Vue({
     subtractFromCart() {
       this.cart -= 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSaleMessage() {
+      if (this.onSale) return this.title + ' is on sale!';
     }
   }
 })
